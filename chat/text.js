0e0cea2b50179085bee1bd47e5d3d176236e63ba
Beatriz Cardoso Ubuntu 24 <btrcardos@gmail.com>
2025-03-08 15:47:09 -0300

    atualiza versao

--- chat/text.js
+++ chat/text.js
@@ -1,30 +1,27 @@
-const axios = require('axios')
-require('dotenv').config();
+const axios = require("axios");
+require("dotenv").config();
 
-async function send(fromId, destinationNumber, messageText){
-    try{
-        let message = await axios({
-            method: "POST",
-            url:
-            "https://graph.facebook.com/v18.0/" +
-            fromId +
-            "/messages",
-            data: {
-                messaging_product: "whatsapp",
-                to: destinationNumber,
-                text: {body: messageText}
-            },
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${process.env.FB_API_TOKEN}`
-            },
-        });
-        console.log("Mensagem respondida");
-        return message;
-    } catch (error) {
-        console.log("Houve um erro ao enviar a mensagem");
-        console.log(error);
-    }
+async function send(fromId, destinationNumber, messageText) {
+  try {
+    let message = await axios({
+      method: "POST",
+      url: "https://graph.facebook.com/v22.0/" + fromId + "/messages",
+      data: {
+        messaging_product: "whatsapp",
+        to: destinationNumber,
+        text: { body: messageText },
+      },
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.FB_API_TOKEN}`,
+      },
+    });
+    console.log("Mensagem respondida");
+    return message;
+  } catch (error) {
+    console.log("Houve um erro ao enviar a mensagem");
+    console.log(error);
+  }
 }
 
-module.exports = {send};
+module.exports = { send };
