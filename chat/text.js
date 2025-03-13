b5734af4f030f464984ce711acce8605f2571853
Beatriz Cardoso Ubuntu 24 <btrcardos@gmail.com>
2025-03-13 20:29:56 -0300

    feat: regex de controle

--- chat/text.js
+++ chat/text.js
@@ -4,7 +4,10 @@ require("dotenv").config();
 const TIME_H = /^([01]?[0-9]|2[0-3])h([0-5][0-9])?|^([01]?[0-9]|2[0-3])h/;
 const TIME_COLON = /^([01]?[0-9]|2[0-3]):[0-5][0-9]/;
 
-const IGNORE_REGEX = /^ignor.*/;
+const IGNORE_REGEX = /^ignor/;
+const SAVE_NOTE_REGEX = /^salv/;
+const DELETE_REGEX = /^(excluir|delete|deletar)/;
+
 const INSULIN_NPH_REGEX = /^[0-9]+\s?(n|nph)$/;
 const INSULIN_R_REGEX = /^[0-9]+\s?(r|reg|regular)$/;
 const INSULIN_UR_REGEX = /^[0-9]+\s?(u\s?r|ultra\s?rapida|fiasp)$/;
@@ -34,6 +37,10 @@ function sanitizeMessage(message) {
 function categorize(message) {
   if (IGNORE_REGEX.test(message)) {
     return "IGNORE";
+  } else if (SAVE_NOTE_REGEX.test(message)) {
+    return "SAVE_NOTE";
+  } else if (DELETE_REGEX.test(message)) {
+    return "DELETE";
   } else if (INSULIN_NPH_REGEX.test(message)) {
     return "INSULIN_NPH";
   } else if (INSULIN_R_REGEX.test(message)) {
