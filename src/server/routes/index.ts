5fe28b4dbfb9566245663a047ea8e0a24e88c562
Beatriz Cardoso Ubuntu 24 <btrcardos@gmail.com>
2025-03-16 12:54:06 -0300

    feat webhookscontroller

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import UsersController from "../controllers/UsersController";
+import WebhooksController from "../controllers/WebhooksController";
 
 const router = Router();
 
 router.get("/", (req, res) => {
   res.sendStatus(200);
 });
+
+router.get("/webhook", WebhooksController.verify);
+router.post("/webhook", WebhooksController.receiveAndSendMessage);
 
 router.post("/users/create", UsersController.create);
 
---
+++ src/server/services/TextService.ts
@@ -0,0 +1,190 @@
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const TIME_H = /^([01]?[0-9]|2[0-3])h([0-5][0-9])?|^([01]?[0-9]|2[0-3])h/;
+const TIME_COLON = /^([01]?[0-9]|2[0-3]):[0-5][0-9]/;
+
+const IGNORE_REGEX = /^ignor/;
+const SAVE_NOTE_REGEX = /^salv/;
+const DELETE_REGEX = /^(excluir|delete|deletar)/;
+
+const INSULIN_NPH_REGEX = /^[0-9]+\s?(n|nph)$/;
+const INSULIN_R_REGEX = /^[0-9]+\s?(r|reg|regular)$/;
+const INSULIN_UR_REGEX = /^[0-9]+\s?(u\s?r|ultra\s?rapida|fiasp)$/;
+
+const GLUCOSE_REGEX =
+  /^(gl(i|u)c?o?s?e?\s?)?\d+\s?(mg\/?dl|(gl(i|u)c?o?s?e?))?$/;
+const SPELLED_HIGH_GLUCOSE_REGEX =
+  /^(hiperg?l?i?c?e?m?i?a?)\s?(leve|braba|grave)?/;
+const SPELLED_LOW_GLUCOSE_REGEX =
+  /^(hipog?l?i?c?e?m?i?a?)\s?(leve|braba|grave)?/;
+
+const BREAKFAST_REGEX = /^cafe/;
+const LUNCH_REGEX = /^almoc/;
+const SNACK_REGEX = /^lanche/;
+const DINNER_REGEX = /^jant/;
+const FREE_MEAL_REGEX = /^refeicao\slivre/;
+const COMPENSATORY_MEAL_REGEX = /^compens/;
+
+const GYM_REGEX = /^(trein|acad(emia)?)/;
+const STRENGTH_TRAINING_REGEX = /^musc(ulacao)?/;
+const AEROBIC_EXERCISE_REGEX = /^((cardio(vascular)?)|(aerobi(o|co)))/;
+// const HEAVY_STRENGTH_TRAINING_REGEX = /^musc(ulacao)?(\s(pesad(o|a)|forte))?/;
+// const LIGHT_STRENGTH_TRAINING_REGEX = /^musc(ulacao)?(\sleve)?/;
+// const HEAVY_AEROBIC_EXERCISE_REGEX =
+//   /^((cardio(vascular)?)|(aerobi(o|co)))(\s(pesad(o|a)|forte))?/;
+// const LIGHT_AEROBIC_EXERCISE_REGEX =
+//   /^((cardio(vascular)?)|(aerobi(o|co)))(\sleve)?/;
+
+function sanitizeMessage(message) {
+  return message
+    .toLowerCase() // Converte para minúsculas
+    .normalize("NFD") // Decompõe caracteres acentuados
+    .replace(/[\u0300-\u036f]/g, "") // Remove acentos
+    .replace(/\s+/g, " ") // Substitui múltiplos espaços por um único
+    .trim(); // Remove espaços extras no início e fim
+}
+
+function categorize(message) {
+  if (IGNORE_REGEX.test(message)) {
+    return "IGNORE";
+  } else if (SAVE_NOTE_REGEX.test(message)) {
+    return "SAVE_NOTE";
+  } else if (DELETE_REGEX.test(message)) {
+    return "DELETE";
+  } else if (INSULIN_NPH_REGEX.test(message)) {
+    return "INSULIN_NPH";
+  } else if (INSULIN_R_REGEX.test(message)) {
+    return "INSULIN_R";
+  } else if (INSULIN_UR_REGEX.test(message)) {
+    return "INSULIN_UR";
+  } else if (GLUCOSE_REGEX.test(message)) {
+    return "GLUCOSE";
+  } else if (SPELLED_HIGH_GLUCOSE_REGEX.test(message)) {
+    return "SPELLED_HIGH_GLUCOSE";
+  } else if (SPELLED_LOW_GLUCOSE_REGEX.test(message)) {
+    return "SPELLED_LOW_GLUCOSE";
+  } else if (BREAKFAST_REGEX.test(message)) {
+    return "BREAKFAST";
+  } else if (LUNCH_REGEX.test(message)) {
+    return "LUNCH";
+  } else if (SNACK_REGEX.test(message)) {
+    return "SNACK";
+  } else if (DINNER_REGEX.test(message)) {
+    return "DINNER";
+  } else if (FREE_MEAL_REGEX.test(message)) {
+    return "FREE_MEAL";
+  } else if (COMPENSATORY_MEAL_REGEX.test(message)) {
+    return "COMPENSATORY_MEAL";
+  } else if (GYM_REGEX.test(message)) {
+    return "GYM";
+  } else if (STRENGTH_TRAINING_REGEX.test(message)) {
+    return "STRENGTH_TRAINING";
+  } else if (AEROBIC_EXERCISE_REGEX.test(message)) {
+    return "AEROBIC_EXERCISE";
+  }
+  return "INDEFINITE";
+}
+
+function separateMatchedAndRemainingParts(str, regex) {
+  const match = str.match(regex);
+  let result;
+
+  if (match) {
+    const matchedPart = match[0];
+    const remainingPart = str.slice(matchedPart.length);
+
+    result = {
+      matchedPart: matchedPart,
+      remainingPart: remainingPart,
+    };
+  } else {
+    result = {
+      matchedPart: null,
+      remainingPart: str,
+    };
+  }
+  return result;
+}
+
+function buildResponse(userPhone, message) {
+  if (typeof message !== "string") {
+    return "Mensagem não suportada.";
+  }
+
+  const tokens = message
+    .slice(0, 100) // primeiros 100 caracteres
+    .split(/[.,]/) // separa tokens por acentos e pontos
+    .filter((token) => token !== ""); // remove tokens vazios
+
+  const data = tokens?.map((dirtyToken) => {
+    let token = sanitizeMessage(dirtyToken);
+    let time = null;
+
+    const startsWithTimeH = separateMatchedAndRemainingParts(token, TIME_H);
+    const startsWithTimeColon = separateMatchedAndRemainingParts(
+      token,
+      TIME_COLON
+    );
+
+    if (startsWithTimeH.matchedPart) {
+      time = startsWithTimeH.matchedPart;
+      token = startsWithTimeH.remainingPart;
+    } else if (startsWithTimeColon.matchedPart) {
+      time = startsWithTimeColon.matchedPart;
+      token = startsWithTimeColon.remainingPart;
+    }
+
+    token = sanitizeMessage(token);
+
+    return {
+      dirtyToken,
+      token,
+      categorie: categorize(token),
+      time,
+    };
+  });
+
+  return (
+    "finalMessage: \n" +
+    data
+      ?.map(
+        (info) =>
+          `dirtyToken: ${info.dirtyToken}\ntoken: ${info.token}\ncategorie: ${info.categorie}\ntime: ${info.time}`
+      )
+      ?.join(" \n\n")
+  );
+}
+
+async function send(fromId, destinationNumber, messageText) {
+  if (process.env.ENV === "DEV") {
+    console.log(
+      `Enviando mensagem de ${fromId} para ${destinationNumber}: ${messageText}`
+    );
+    return;
+  }
+  try {
+    let message = await axios({
+      method: "POST",
+      url: `https://graph.facebook.com/${process.env.FB_API_VERSION}/${fromId}/messages`,
+      data: {
+        messaging_product: "whatsapp",
+        to: destinationNumber,
+        text: { body: messageText },
+      },
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.FB_API_TOKEN}`,
+      },
+    });
+    console.log("Mensagem respondida");
+    return message;
+  } catch (error) {
+    console.log("Houve um erro ao enviar a mensagem");
+    console.log(error);
+  }
+}
+
+export default { send, buildResponse };
