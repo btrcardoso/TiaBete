61c8af599bca0eb7b21eab9534726e12be6c6a62
Beatriz Cardoso Ubuntu 24 <btrcardos@gmail.com>
2025-03-16 17:48:59 -0300

    remove comments

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,14 +3,6 @@ import express from "express";
 import bodyParser from "body-parser";
 import { router } from "./routes";
 
-//import providers from "./providers.js";
-// import chat from "./chat/index.js";
-// // import media from "./media/index.js";
-// //import file from "./utils/file.js";
-// //import time from "./utils/converTime.js";
-// import feedbacks from "./feedbacks/feedbacks.js";
-// import mongodb from "./mongoDB/index.js";
-
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
